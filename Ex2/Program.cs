eb956cae54271a8631b910ed87593d8d459ae54a
Kleo <mmttdd16@gmail.com>
2023-09-23 08:04:34 +0300

    Finished Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,4 +1,34 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
-System.Console.WriteLine();
+  
+  
+
+
+
+        Random random = new Random();
+        int arrayLength = random.Next(1, 100); // Генерируем случайную длину массива от 1 до 99
+        int[] myArray = new int[arrayLength]; // Создаём массив заданной длины
+
+
+        // Заполняем массив случайными трёхзначными числами
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] = random.Next(1, 100); // Генерируем случайное двухзначное число
+        }
+
+        // Выводим элементы массива на экран
+        Console.WriteLine("Сгенерированный массив:");
+        foreach (int number in myArray)
+        {
+            Console.Write(number + " ");
+        }
+
+        System.Console.WriteLine();
+        int sum = 0;
+        for (int i = 1; i < myArray.Length; i += 2)
+        {
+            sum += myArray[i];
+        }
+
+        Console.WriteLine("Сумма элементов на нечётных позициях: " + sum);
